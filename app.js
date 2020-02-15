c5cf592b452072405080ce4fce185ac0ab50920a
super1or <38477985+super1or@users.noreply.github.com>
2020-02-15 23:10:58 +0000

    Name changed to Loan calculator, added some minor functionallity and error msg

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // Listener for submit
-document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+document.querySelector('#loan-form').addEventListener('submit', function (e) {
+    // Hide results
+    document.getElementById('results').style.display = 'none';
 
-// Cacl Res
+    // Show loader
+    document.getElementById('loader').style.display = 'block';
 
-function calculateResults(e) {
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
+
+// Calculate Results
+
+function calculateResults() {
     // UI variables
     const amount = document.querySelector('#amount');
     const interest = document.querySelector('#interest');
@@ -19,15 +29,43 @@ function calculateResults(e) {
     // Compute monthly payment
 
     const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-    const monthly = (principal*x*calculatedInterest)/(x-1);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
 
     if (isFinite(monthly)) {
-        monthlyPayment.value = monthly.toFixed(0);
-        totalPayment.value = (monthly*calculatedPayments).toFixed(0);
-        totalInterest.value = ((monthly*calculatedInterest)-principal).toFixed(0);
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly*calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        // Show results
+        document.getElementById('results').style.display = 'block';
+        // Hide loader
+        document.getElementById('loader').style.display = 'none';
     } else {
-        console.log('Please, check your numbers');
+        showError('Please check your numbers input');
     }
+}
 
-    e.preventDefault();
+// Display Error
+function showError(error) {
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+    // Hide loader
+    document.getElementById('loader').style.display = 'none';
+
+    // Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+    // Add a div
+    const errorDiv = document.createElement('div');
+    // Add a class
+    errorDiv.className = 'alert alert-danger';
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+    // Clear error after awhile
+    setTimeout(clearError, 3000);
+}
+
+function clearError() {
+    document.querySelector('.alert').remove();
 }
